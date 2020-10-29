d82cea4d4285fa3419a996b92694686e4a09536f
ohamjoseph <ouilyh@gmail.com>
2020-10-29 11:42:09 +0000

    Updates

---
+++ precache-manifest.753b815b74962725f78f413d91d10509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed64b3fc732d1461572f414b0d84630",
+    "url": "/mobileFirst/index.html"
+  },
+  {
+    "revision": "dc7ff7c73c898de02121",
+    "url": "/mobileFirst/static/css/2.6a770793.chunk.css"
+  },
+  {
+    "revision": "32d9f607843127052866",
+    "url": "/mobileFirst/static/css/main.ceb931c7.chunk.css"
+  },
+  {
+    "revision": "dc7ff7c73c898de02121",
+    "url": "/mobileFirst/static/js/2.cd132385.chunk.js"
+  },
+  {
+    "revision": "da1ddcda7183b6313ab6bca1da290ddd",
+    "url": "/mobileFirst/static/js/2.cd132385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d9f607843127052866",
+    "url": "/mobileFirst/static/js/main.4ac36f85.chunk.js"
+  },
+  {
+    "revision": "df8ad0fff5684979f80d",
+    "url": "/mobileFirst/static/js/runtime-main.b5fe4c7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mobileFirst/precache-manifest.3823a3c28584033947988720301d0ccd.js"
+  "/mobileFirst/precache-manifest.753b815b74962725f78f413d91d10509.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mobileFirst/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
